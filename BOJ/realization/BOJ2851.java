a683bbfdb033d469472a52060a125968f8657613
dydgjs200 <dydgjs2016@naver.com>
2020-07-22 12:55:08 +0900

    Update BOJ2851.java

--- BOJ/realization/BOJ2851.java
+++ BOJ/realization/BOJ2851.java
@@ -18,15 +18,15 @@ public class BOJ2851 {
         for (int i = 0; i < 10; i++) {
             array[i] = scanner.nextInt();
             score += array[i];
-            if (score >= 100) {
+            if (score >= 100) {     //score가 기준인 100을 넘는다면 종료시킴
                 cal1 = score;
                 break;
             }
-            cal2 = score;
+            cal2 = score;   //100을 넘지 못할 때의 최대값이 cal2가 된다.
         }
-        if (cal1 == cal2)
+        if (cal1 == cal2)   //같다면 더 큰숫자를 출력(cal1)
             System.out.print(cal1);
-        else {
+        else {          // 100과 차이가 더 적은 숫자를 판별해서 출력
             System.out.print(cal1 - 100 < 100 - cal2 ? cal1 : cal2);
         }
     }
