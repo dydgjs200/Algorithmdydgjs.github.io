c399b3209f610efe15096a7a5e5838edb152d029
dydgjs200 <dydgjs2016@naver.com>
2020-08-03 18:22:31 +0900

    Add files via upload

---
+++ Create Algorithm & Theory/find_string.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.HashMap;
+
+public class find_string {
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        String target = "abcdefasg";
+        String find = "efa";
+        boolean res = true;
+        int result = 0;
+
+        for(int i=0; i<target.length(); i++){
+            if(target.charAt(i) == find.charAt(0)){
+                int pos1 = i;   //pos1은 타겟에
+                int pos2 = 0;   //pos2는 find에 대입
+                while(res){
+                    if(pos1 < target.length() && pos2 < find.length()){
+                        if(target.charAt(pos1) != find.charAt(pos2))
+                            res = false;
+                        else {
+                            pos1++;
+                            pos2++;
+                            if(pos2 >= find.length()) {  //찾는 인덱스가 끝에 도달하면 result=1
+                                result = 1;
+                                res = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if(result == 1){
+                System.out.print("find!");
+                break;
+            }
+        }
+
+    }
+}
